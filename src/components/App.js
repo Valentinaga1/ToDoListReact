2ddbd48fae6868b3769dd86bce63809f1c2526d2
Valentinaga1 <1881@holbertonschool.com>
2021-07-12 10:11:01 -0500

    Fixing details

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,19 @@ import '../App.css';
 
 const App = () => {
   // hooks
-  const[todos, setTodos] = React.useState([]);
-  const[todo, setTodo] = React.useState('');
-  const[todoEditing, setTodoEditing] = React.useState(null);
-  const[editingText, setEditingText] = React.useState('');
+  const [todos, setTodos] = React.useState([]);
+  const [todo, setTodo] = React.useState('');
+  const [todoEditing, setTodoEditing] = React.useState(null);
+  const [editingText, setEditingText] = React.useState('');
 
   React.useEffect(() => {
     const temp = localStorage.getItem("todos");
     const loadedTodos = JSON.parse(temp);
 
-    if(loadedTodos){
+    if (loadedTodos) {
       setTodos(loadedTodos);
     }
-  },[])
+  }, [])
 
   React.useEffect(() => {
     // stringify converts any js into json
@@ -23,7 +23,7 @@ const App = () => {
     localStorage.setItem("todos", temp);
   }, [todos])
 
-  const handleSubmit = (e) =>{
+  const addTodo = (e) => {
     // To prevent the refresh of the web page
     e.preventDefault();
 
@@ -32,8 +32,7 @@ const App = () => {
       text: todo,
       completed: false,
     }
-
-    // creating the new array with the spread operator to clone todos variable withput mutate the original data
+    // creating the new array with the spread operator to clone todos variable without mutate the original data
     setTodos([...todos].concat(newTodo));
     // to reset the input and make it blank
     setTodo("");
@@ -45,20 +44,19 @@ const App = () => {
     setTodos(updatedTodos);
   }
 
-  const toggleComplete = (id) => {
+  const completeTodo = (id) => {
     const updatedTodos = [...todos].map((todo) => {
-      if(todo.id === id){
+      if (todo.id === id) {
         todo.completed = !todo.completed;
       }
       return todo;
     })
-
     setTodos(updatedTodos);
   }
 
   const editTodo = (id) => {
     const updatedTodos = [...todos].map((todo) => {
-      if(todo.id === id){
+      if (todo.id === id) {
         todo.text = editingText;
       }
       return todo;
@@ -69,34 +67,34 @@ const App = () => {
 
   return (
     <div id="todo-list" className="App">
-     <h1>Todo List</h1>
+      <h1>Todo List</h1>
 
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={(e) => {setTodo(e.target.value)}} value={todo} />
+      <form onSubmit={addTodo}>
+        <input type="text" onChange={(e) => { setTodo(e.target.value) }} value={todo} />
         <button type="submit" >Add Task</button>
       </form>
 
-      {todos.map((todo) => 
+      {todos.map((todo) =>
         <div key={todo.id} className="todo">
           <div className="todo-text">
-          <input 
-            type="checkbox"
-            onChange={() => toggleComplete(todo.id)} 
-            checked={todo.completed}/>
+            <input
+              type="checkbox"
+              onChange={() => completeTodo(todo.id)}
+              checked={todo.completed} />
 
-          {todoEditing === todo.id ? 
-            (<input 
-              type="text" 
-              onChange={(e)=> setEditingText(e.target.value)}
-              value={editingText}/>) : 
+            {todoEditing === todo.id ?
+              (<input
+                type="text"
+                onChange={(e) => setEditingText(e.target.value)}
+                value={editingText} />) :
               (<div>{todo.text}</div>)}
           </div>
-          
+
           <div className="todo-actions">
-            {todoEditing === todo.id ? 
-            (<button onClick={() => editTodo(todo.id)} >Submit edit</button>) : 
-            (<button onClick={() => setTodoEditing(todo.id)}>Edit</button>)}
-            <button onClick={() => {deleteTodo(todo.id)}}>Delete</button>
+            {todoEditing === todo.id ?
+              (<button onClick={() => editTodo(todo.id)} >Submit edit</button>) :
+              (<button onClick={() => setTodoEditing(todo.id)}>Edit</button>)}
+            <button onClick={() => { deleteTodo(todo.id) }}>Delete</button>
           </div>
 
         </div>)}
